2a75e4422d34655594cf6a2d3743f4958e18a05f
Joao Bernardo <jb.amaro95@gmail.com>
2016-10-15 14:13:46 +0100

    Adds username length validation

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,6 @@
 class Score < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true,
+                       length: { in: 1..40 }
   validates :text_id, presence: true
   validates :wpm, presence: true
   validates :time, presence: true
